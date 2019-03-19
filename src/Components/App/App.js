ed3cdb9c7387abc57a1c2de75413ec66e995002b
Joshua Lavarine <joshua@lavarine.com>
2019-03-19 14:43:07 -0600

    Create fetch for movies and setup gitignore
    Co-authored-by: David Cisneros <Dcisneros@gmail.com>

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
+import { url } from '../../apiURL';
 
 export default class App extends Component {
+
+  componentDidMount() {
+    this.fetchNowPlaying(url);
+  }
+
+  fetchNowPlaying = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    const movies = data.results;
+    console.log(movies)
+    // assign movies to store
+  }
+
   render() {
     return (
       <div className="App">
