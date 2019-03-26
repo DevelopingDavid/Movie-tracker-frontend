5cecb70d0bb8fe6536e17cb76ec86ebe3e31379c
Joshua Lavarine <joshua@lavarine.com>
2019-03-26 16:35:12 -0600

    Resume testing of Header and MoviePopup

--- src/containers/MoviePopup/MoviePopup.test.js
+++ src/containers/MoviePopup/MoviePopup.test.js
@@ -1,17 +1,54 @@
 import React from 'react';
-import MoviePopup from '../MoviePopup/MoviePopup';
+import { MoviePopup, mapDispatchToProps, mapStateToProps} from '../MoviePopup/MoviePopup';
 import { shallow } from 'enzyme';
+import { setFavoriteMovies, loginUser } from '../../actions';
 
 describe('MoviePopup', () => {
   let wrapper;
+  let mockState = {
+    user: {
+      id: 76,
+      name: 'matt'
+    }
+  }
+
+  let mockFoundMovie = {}
 
   beforeEach(() => {
     wrapper = shallow( 
-      <MoviePopup/>
+      <MoviePopup user={mockState}
+                  foundMovie={mockFoundMovie}/>
     ) 
   });
 
   it('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  describe('mapStateToProps', () => {
+    it('should return a user object', () => {
+      const predicted = {
+        user: {
+          id: 76,
+          name: 'matt'
+      }}
+      
+      const mappedProps = mapStateToProps(mockState);
+      
+      expect(mappedProps).toEqual(predicted)
+    });
+  });
+
+  // describe('mapDispatchToProps', () => {
+  //   it('should call dispatch when using mapDispatchToProps', () => {
+  //     const mockDispatch = jest.fn();
+  //     const actionToDispatch = setFavoriteMovies(1);
+
+  //     const mappedProps = mapDispatchToProps(mockDispatch);
+      
+  //     mappedProps.setFavoriteMovies(1);
+
+  //     expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+  //   });
+  // });
 });
