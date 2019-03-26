4fa3d0da9356edb7f7aca1e0c889cc622e2805f6
Joshua Lavarine <joshua@lavarine.com>
2019-03-26 08:51:55 -0600

    Complete tests for loginUserReducer

--- src/reducers/reducersTests/loginUserReducer.test.js
+++ src/reducers/reducersTests/loginUserReducer.test.js
@@ -7,4 +7,20 @@ describe('loginUserReducer', () => {
     const result = loginUserReducer(undefined, {});
     expect(result).toEqual(predicted);
   });
+
+  it('should return the state with new movies', () => {
+    const mockUser = {
+      id: 1,
+      name: 'Josh David'
+    };
+    const intialState = [];
+    const predicted = mockUser;
+
+    const action = actions.loginUser(mockUser.id, mockUser.name);
+    const result = loginUserReducer(intialState, action);
+
+    expect(result).toEqual(predicted);
+
+  });
+
 });
